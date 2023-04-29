d2f39fdcd4161ceefbee665bbeefdb2ba806d98e
Vendula Krajíčková <vendysacek@seznam.cz>
2023-04-29 16:16:52 +0200

    change class to id

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-console.log('funguju!');
+// console.log('funguju!');
 
 // fetch('https://api.sunrise-sunset.org/json?lat=50&lng=14.5')
 //     .then((response) => response.json())
 //     .then((data) => {
 //         console.log(data);
-//         document.querySelector('.vychod').innerHTML = `Východ slunce: ${data.results.sunrise}`;
-//         document.querySelector('.zapad').innerHTML = `Západ slunce: ${data.results.sunset}`;
+//         document.querySelector('#vychod').innerHTML = `Východ slunce: ${data.results.sunrise}`;
+//         document.querySelector('#zapad').innerHTML = `Západ slunce: ${data.results.sunset}`;
 //     })
 
-const buttonElm = document.querySelector('.button');
+const buttonElm = document.querySelector('#button');
 
 const zobraz = () => {
     // console.log('klik');
@@ -16,8 +16,8 @@ const zobraz = () => {
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
-        document.querySelector('.vychod').innerHTML = `Východ slunce: ${data.results.sunrise}`;
-        document.querySelector('.zapad').innerHTML = `Západ slunce: ${data.results.sunset}`;
+        document.querySelector('#vychod').innerHTML = `Východ slunce: ${data.results.sunrise}`;
+        document.querySelector('#zapad').innerHTML = `Západ slunce: ${data.results.sunset}`;
     })
 }
 
